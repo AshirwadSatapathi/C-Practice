6556cfacadaad7fa5157e5def4ca7d3d34940897
Ashirwad Satapathi <ashirwadsatapathi.aim@gmail.com>
2019-08-08 23:35:03 +0530

    Adding Files of Method Overloading

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1 = add(10, 12);
+            int result2 = add(10,12,12);
+
+            Console.WriteLine("{0},{1}",result1,result2);
+            Console.ReadLine();
+         }
+        public static int add(int a, int b) {
+            return a + b;
+        }
+        public static int add(int a,int b,int c)
+        {
+            return a + b + c;
+        }
+    }
+}
